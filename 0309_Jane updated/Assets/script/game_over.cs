2d602289ff2cbdb8fc271bb103bd7b9c7932d298
EmmaYChen <yaochen.jo@gmail.com>
2018-03-20 01:12:07 -0700

    Emma house collider

---
+++ 0309_Jane updated/Assets/script/game_over.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_over : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "leepy") {
+			Debug.Log ("snowball hit house");
+			// game over
+		}
+	}
+}
